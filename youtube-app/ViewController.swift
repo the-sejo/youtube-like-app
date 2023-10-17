a1c1924d8be59e1867187805b1486195e37e875c
Senjer Celiktas <apple@Apples-MacBook-Pro-2.local>
2023-10-17 19:03:22 +0300

    initial commit

---
+++ youtube-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-app
+//
+//  Created by Apple on 2023-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
